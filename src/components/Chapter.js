b603f8fab53be47e690ed0cc0747e03286d0aefe
Gabriel Morais <gabmla19@gmail.com>
2018-11-17 17:59:22 -0300

    :blossom: chapter improvements :blossom:

--- src/components/Chapter.js
+++ src/components/Chapter.js
@@ -1,43 +1,28 @@
-import React, { Component } from 'react'
-import Moment from 'moment';
+import React, { Component } from "react";
+import Moment from "moment";
 import { Container, Header, Divider, Label } from "semantic-ui-react";
 
-import './Chapter.scss';
+import "./Chapter.scss";
 
 export default class Chapter extends Component {
+  constructor(props) {
+    super(props);
 
-  constructor() {
-    super();
-
-    this.state = {
-      chapter: {text: ""}
-    }
-  }
-
-  componentDidMount() {
-    this.setState({
-      chapter: this.props.data
-    }, () => console.log('chapter', this.state));
+    this.state = { chapter: props.data };
   }
 
   render() {
-    Moment.locale('pt');
+    Moment.locale("pt");
     let chapter = this.state.chapter;
-    
+
     return (
       <div className="chapter">
-        <Container className="container" text>
+        <Container className="chapter-container" textAlign="justified" text>
           <Divider />
-          <Header as='h2' inverted>{chapter.title}</Header>
+          <Header className="chapter-title" as="h3" inverted>
+            {chapter.title}
+          </Header>
           <p>{chapter.text}</p>
-          <Label basic color="violet">
-            Characters
-            <Label.Detail>{chapter.text.length}</Label.Detail>
-          </Label>
-          <Label color="violet">
-            Created at
-            <Label.Detail>{Moment(chapter.created_at).format('DD/MM/YYYY')}</Label.Detail>
-          </Label>
         </Container>
       </div>
     );
