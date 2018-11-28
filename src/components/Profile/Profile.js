281c1c16e52660378d48409420bbddb46231182e
Gabriel Morais <gabmla19@gmail.com>
2018-11-28 17:55:44 -0300

    :ear_of_rice: profile author :ear_of_rice:

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -1,8 +1,9 @@
 import React, { Component } from "react";
+import Author from "../Fic/Author";
 import API from '../../api';
 
 import "./Profile.scss";
-import { Container, Grid, Loader } from 'semantic-ui-react';
+import { Container, Grid, Loader, Menu, Input, Button } from 'semantic-ui-react';
 
 export default class Profile extends Component {
   constructor(props) {
@@ -12,7 +13,8 @@ export default class Profile extends Component {
       isLoading: true,
       profileName: props.match.params.profile_name,
       authProfile: false,
-      profile: {}
+      profile: {},
+      activeItem: 'fics'
     }
   }
 
@@ -28,11 +30,45 @@ export default class Profile extends Component {
       .finally(() => this.setState({ isLoading: false }));
   }
 
+  handleItemClick = (e, { name }) => this.setState({ activeItem: name })
+
   render() {
+    const { activeItem } = this.state
     if (this.state.isLoading) {
       return <Loader active={this.state.isLoading} inverted />
     } else {
-      return <div>EU sou um profile muy lindo.</div>;
-    }
+      return (
+      <div className="fic">
+        <Grid stackable columns={2} divided inverted>
+          <Grid.Row>
+            <Grid.Column width={4} only="mobile tablet computer">
+              <Author data={this.state.profile} />
+              <Button style={{ marginTop: 1 + "rem" }} fluid color="violet" inverted active>Edit Profile</Button>
+            </Grid.Column>
+
+            <Grid.Column width={12}>
+              <Menu className="no-border" pointing secondary inverted>
+                <Menu.Item name='fics' active={activeItem === 'fics'} onClick={this.handleItemClick} />
+                <Menu.Item
+                  name='chapters'
+                  active={activeItem === 'chapters'}
+                  onClick={this.handleItemClick}
+                />
+                <Menu.Item
+                  name='favorites'
+                  active={activeItem === 'favorites'}
+                  onClick={this.handleItemClick}
+                />
+                <Menu.Menu position='right'>
+                  <Menu.Item>
+                    <Button>oi</Button>
+                  </Menu.Item>
+                </Menu.Menu>
+              </Menu>
+            </Grid.Column>
+          </Grid.Row>
+        </Grid>
+      </div>
+      )}
   }
 }
