7cb493873982477511f178e0cb3a3e42a990f3f4
Gabriel Morais <gabmla19@gmail.com>
2018-12-15 19:43:39 -0300

    :banana: search on home page :banana:

--- src/components/Home/SearchFics.js
+++ src/components/Home/SearchFics.js
@@ -1,15 +1,19 @@
 import React, { Component } from "react";
-
+import _ from "lodash";
+import { ficUrl } from "../../static/api";
+import axios from "axios";
 import "./SearchFics.scss";
-import { Search } from "semantic-ui-react";
+import { Search, Label } from "semantic-ui-react";
 
 export default class SearchFics extends Component {
   constructor() {
     super();
 
     this.state = {
-      searchField: { isLoading: false, results: [], value: "" },
-      other: {}
+      isLoading: false,
+      results: [],
+      value: "",
+      hasResults: false
     };
   }
 
@@ -17,14 +21,15 @@ export default class SearchFics extends Component {
     this.resetSearchComponent();
   }
 
-  resetSearchComponent = () =>
+  resetSearchComponent = () => {
+    console.log("resetando");
     this.setState({
-      searchField: {
-        isLoading: false,
-        results: [],
-        value: ""
-      }
+      isLoading: false,
+      results: [],
+      value: "",
+      hasResults: false
     });
+  };
 
   handleResultSelect = (e, { result }) =>
     this.setState({
@@ -32,22 +37,47 @@ export default class SearchFics extends Component {
     });
 
   handleSearchChange = (e, { value }) => {
-    if (value.length > 0) {
-      this.setState({ searchField: { isLoading: true, value } });
-    } else {
-      return this.resetSearchComponent();
-    }
+    this.setState(
+      {
+        isLoading: true,
+        value: value
+      },
+      () => {
+        if (this.state.value.length < 1) return this.resetSearchComponent();
+      }
+    );
+
+    axios
+      .get(ficUrl + "search?title=" + value)
+      .then(response => {
+        const fics = response.data.fics;
+        let fic = { title: fics[0].title, description: fics[0].category };
+
+        this.setState({
+          results: fics,
+          hasResults: fics.length > 0
+        });
+      })
+      .catch(error => {
+        this.setState({ results: [], hasResults: [].length > 0 });
+      })
+      .finally(() => {
+        this.setState({ isLoading: false });
+      });
   };
 
   render() {
-    let { isLoading, value, results } = this.state.searchField;
+    let { isLoading, value, results, hasResults } = this.state;
     return (
       <Search
         className="search-box"
         size="big"
+        placeholder="Discover Fics!"
         loading={isLoading}
         onResultSelect={this.handleResultSelect}
-        onSearchChange={this.handleSearchChange}
+        onSearchChange={_.debounce(this.handleSearchChange, 500, {
+          leading: true
+        })}
         results={results}
         value={value}
         fluid
